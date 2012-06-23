57b0887f4e02986634bccd55362a05f2b4b29927
Michael Czeraszkiewicz <czerasz.hosting@gmail.com>
2012-06-24 00:44:24 +0200

    Updated the basebox and .gitignore.
    Added screen and curl packages.
    Added update and initialize abstraction.
    Fixed the iubuntu locale bug.
    
    Added .vagrant to .gitignore
    
    	modified:   .gitignore
    	modified:   puppet/modules/boxes/manifests/base.pp

--- puppet/modules/boxes/manifests/base.pp
+++ puppet/modules/boxes/manifests/base.pp
@@ -1,8 +1,20 @@
-class boxes::base ($vm_name="base") {
+class boxes::base::update () {
+	# if ($operatingsystem == "Ubuntu") and ($operatingsystemrelease == 10.04) {
+	#     notify {"You are on Ubuntu 10.04, and that is why we need to update first": }
 
+	# 		exec { "apt-get update":
+	# 		  alias => "update_ubuntu_10_04",
+	# 		  path => "/usr/bin",
+	# 		  creates => "/etc/apt/sources.list.d/nginx-stable-natty.list",
+	# 		}
+	# 	}
+ #  }
+}
+
+class boxes::base::init ($vm_name="base") {
 	Exec { path => ["/bin", "/sbin", "/usr/bin", "/usr/sbin"] }
 
-	package { ["vim", "tree"]:
+	package { ["vim", "tree", "screen", "curl"]:
 		ensure => present
 	}
 
@@ -11,4 +23,19 @@ class boxes::base ($vm_name="base") {
 	  ensure  => file,
 	  replace => true,
 	}
+
+	if ($operatingsystem == "Ubuntu") and ($operatingsystemrelease == 10.04) {
+		exec { "fix locale bug": 
+			command => "echo 'LC_ALL=\"en_GB.UTF-8\"' >> /etc/default/locale"
+		}
+	}
+}
+
+class boxes::base ($vm_name="base") {
+	include boxes::base::update
+	class { "boxes::base::init":
+		vm_name => $vm_name,
+	}
+
+	Class["boxes::base::update"] -> Class["boxes::base::init"]
 }
